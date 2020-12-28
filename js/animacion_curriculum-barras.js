3990363383285de4e1cdcce3095b6b5122e0f42f
Daniel50313 <76673360+Daniel50313@users.noreply.github.com>
2020-12-27 22:17:52 -0600

    Add files via upload

---
+++ js/animacion_curriculum-barras.js
@@ -0,0 +1,23 @@
+window.onload = function() { 
+		
+		animateprogress("#html5",70);
+		animateprogress("#CSS",65);
+		animateprogress("#sql",70);
+		animateprogress("#cshar",65);
+		
+	} 	
+	document.querySelector ("#boton").addEventListener ("click", function() { 
+		animateprogress("#html5",91);
+		animateprogress("#CSS",72);
+		animateprogress("#sql",86);
+		animateprogress("#cshar",52);
+   
+	});
+
+	window.onload = function() {
+    animateprogress("#html5",91);
+	animateprogress("#CSS",72);
+	animateprogress("#sql",86);
+	animateprogress("#cshar",52);
+   
+}
